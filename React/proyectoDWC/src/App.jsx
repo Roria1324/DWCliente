69dccf92fdedb587a1aa38a945623dafada27324
Gomez <jgomez@localhost>
2025-09-18 19:03:06 +0200

    UT02 Código de clase

--- React/proyectoDWC/src/App.jsx
+++ React/proyectoDWC/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
 import logoReact from "./assets/react.svg";
+import Feos from "./components/Feos.jsx";
+import Horrible from "./components/Horrible.jsx";
 
 function App() {
   // Código JavaScript Vanilla.
@@ -12,6 +14,10 @@ function App() {
       <img width={ancho} src={logoReact}></img>
       <h2>Desarrollo Web en Entorno Cliente.</h2>
       <h3>¡Hola, Feos!</h3>
+      <Feos nombre='Juan Carlos' apellidos='Feo de Verdad' tamanyo={ancho}>
+        Hola soy childen.
+        <Horrible />
+      </Feos>
     </>
   );
 }
