f53e3c4675a12190d21c1f4f7576d8d2609ccc0f
alex <ciclost@localhost>
2025-09-19 16:05:22 +0200

    Proyecto Cuevana Añadido

---
+++ React/Cuevana/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Contenedor from './Componentes/Contenedor.jsx'
+
+function App() {
+  return (
+    <>
+      <Contenedor>
+        <p>Contenedor</p>
+      </Contenedor>
+    </>
+  )
+}
+
+export default App
