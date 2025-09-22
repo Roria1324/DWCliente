4b9416e28b690dba1d33ec70620086f3729241e7
alex <ciclost@localhost>
2025-09-22 19:34:44 +0200

    Terminado cuevana

--- React/Cuevana/src/Componentes/Interprete.jsx
+++ React/Cuevana/src/Componentes/Interprete.jsx
@@ -6,9 +6,11 @@ const Interprete = (props) => {
     return (
         <>
             <div className="interprete-first">
-            <h1>{props.nombre}</h1>
-            <img src={props.foto} alt="imagen"/>
-            <p>{props.children}</p>
+                <div className="foto"><img src={props.foto} alt="imagen"/></div>
+                <div className="info">
+                    <div className="nombre"><h1>{props.nombre}</h1></div>
+                    <div><p>{props.bibliografía}</p></div>
+                </div>
             </div>
         </>
     )
