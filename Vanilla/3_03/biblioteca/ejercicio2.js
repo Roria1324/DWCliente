aacc079fcb36303eb125d1084c51ef16284fb609
alex <ciclost@localhost>
2025-09-23 18:56:53 +0200

    Empezado ejer2 no funciona export

---
+++ Vanilla/3_03/biblioteca/ejercicio2.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const createCourseNew = (course, year, description) => {
+    return `El curso de ${course} del año ${year} es ${description} y tiene ${this.student.length} estudiantes inscritos.`;
+}
+
+export default createCourseNew;
--- Vanilla/3_03/main.js
+++ Vanilla/3_03/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
 import { createCourse } from "./biblioteca/ejercicio1.js";
+import { createCourseNew } from "./biblioteca/ejercicio2.js";
 
-console.log(createCourse());
+
+console.log(createCourse);
+
+console.log(createCourseNew("Alex", "2025", "Quiero terminar el curso"));
