51c704ce5df478e5f99db55e1aa569111d6c54a7
jucagovi <jc.gomez@edu.gva.es>
2025-09-26 10:58:19 +0200

    UT03 Ampliación ejemplo objetos con 'metodos'

---
+++ Vanilla/UT03/principal_objetos.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const clienteFeo = {
+  nombre: "Nombre feo",
+  apellidos: "Apellidos feos",
+  saldo: 345.89,
+  articulos: [],
+};
+
+const articuloFeo = {
+  nombre: "Libro Feo",
+  precio: 19.9,
+  descripcion: "Descripción fea",
+};
