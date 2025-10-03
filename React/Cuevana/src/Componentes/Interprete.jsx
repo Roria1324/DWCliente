17ff38688858bac5c0d526cd0b71bbaf448e78bd
alex <ciclost@localhost>
2025-10-03 15:50:36 +0200

    Empezado ejercicio 3.08

--- React/Cuevana/src/Componentes/Interprete.jsx
+++ React/Cuevana/src/Componentes/Interprete.jsx
@@ -5,22 +5,20 @@ const Interprete = (props) => {
 
     return (
         <>
-<<<<<<< HEAD
             <div className="interprete-container">
-                <div className="interprete-name"><h1>{props.nombre}</h1></div>
-            <img src={props.foto} alt="imagen"/>
-            <p>{props.children}</p>
-=======
-            <div className="interprete-first">
-                <div className="foto"><img src={props.foto} alt="imagen"/></div>
-                <div className="info">
-                    <div className="nombre"><h1>{props.nombre}</h1></div>
-                    <div><p>{props.bibliografía}</p></div>
+                    <div className="interprete-name"><h1>{props.nombre}</h1></div>
+                <img src={props.foto} alt="imagen"/>
+                <p>{props.children}</p>
+                <div className="interprete-first">
+                    <div className="foto"><img src={props.foto} alt="imagen"/></div>
+                    <div className="info">
+                        <div className="nombre"><h1>{props.nombre}</h1></div>
+                        <div><p>{props.bibliografía}</p></div>
+                    </div>
                 </div>
->>>>>>> 95afdbcc4b9b619891f9d757db9b98d19772665d
             </div>
         </>
     )
 }
 
-export default Interprete
+export default Interprete;
---
+++ React/estadoreact/src/componentes/listado.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+const listado = () => {
+
+    const numerAleatorio = Math.floor(Math.random() + 1)
+
+    return (
+        <>
+        
+        </>
+    )
+}
