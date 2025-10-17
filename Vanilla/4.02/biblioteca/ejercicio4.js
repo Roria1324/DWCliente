999e210def2f46e36577395d6dd0dc117841c19a
alex <ciclost@localhost>
2025-10-17 19:29:25 +0200

    Ejercicio 4.02 terminado

--- Vanilla/4.02/biblioteca/ejercicio4.js
+++ Vanilla/4.02/biblioteca/ejercicio4.js
@@ -1,23 +1,33 @@
 "use strict";
 
-var imagen = 
-    ["",
-    "",
-    "",
-    "",]
+var imagen = ["./Imagenes/espin.jpeg",
+    "./Imagenes/oso.jpeg",
+    "./Imagenes/planta.jpeg",
+    "./Imagenes/rata.jpeg"];
 
 const carruselImagenes = () => {
     const contenedor = document.createElement("div");
     const carrusel = document.createElement("img");
+    
+    carrusel.setAttribute("id", "carrusel"); 
+
     contenedor.appendChild(carrusel);
     document.body.appendChild(contenedor);
 
+    carrusel.style.height = "600px";
+
+    carrusel.style.width = "500px";
     let i = 0;
-    imagen.src = imagen[i];
+    carrusel.src = imagen[i];
 
     setInterval(() => {
-    i = i++ % imagen.length;
-    imagen.src = imagen[i];
+    i = (i + 1) % imagen.length;
+    carrusel.src = imagen[i];
+    
+    const elemento = document.getElementById("carrusel");
+    if (elemento){
+        elemento.src = imagen[i]
+    }
     }, 2000)
 }
 
