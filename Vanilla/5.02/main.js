60e7d407901ad6317d81f4aa1142c02f1807065a
Roria <roriarumanosky@gmail.com>
2025-10-30 13:29:39 +0100

    Empezado ejercicio2 5.02

--- Vanilla/5.02/main.js
+++ Vanilla/5.02/main.js
@@ -1,8 +1,10 @@
 "use strict";
 
 import acordeon from "./biblioteca/ejericicio1.js";
+import pestañas from "./biblioteca/ejericicio2.js"
 
 
  document.addEventListener("DOMContentLoaded", () => {
-   acordeon();
+   acordeon()
+   pestañas()
  })
