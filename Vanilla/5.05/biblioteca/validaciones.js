ce1189fa1a8b4c6c62d4e317550ef6c521f1385f
alex <ciclost@localhost>
2025-11-13 18:11:53 +0100

    Seguir 5.05

--- Vanilla/5.05/biblioteca/validaciones.js
+++ Vanilla/5.05/biblioteca/validaciones.js
@@ -1,2 +1,24 @@
 "use strict";
 
+const nombreValidar = (nombre) => {
+    if (nombre.length < 5) return false;
+    if (nombre === "") return false;
+}
+
+const grupoMusicalValidar = (nombre) => {
+    if(nombre.length < 5) return false;
+    if(nombre === "") return false;
+}
+
+const yearPublicacionValidar = (year) => {
+    if(year.length < 4 || year.length > 4) return false;
+    if(year === "") return false;
+}
+
+const generoValidar = (genero) => {
+    const patron = /'[ES-][0-9]'/ 
+}
+
+const localizacionValidar = (codigo) => {
+
+}
