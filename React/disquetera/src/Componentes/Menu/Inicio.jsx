d57a2041436477167c4c724c34dedd677ad1ad35
Roria <roriarumanosky@gmail.com>
2025-11-20 13:42:27 +0100

    Empezado 5.06

---
+++ React/disquetera/src/Componentes/Menu/Inicio.jsx
@@ -0,0 +1,15 @@
+"use strict";
+import React from 'react'
+
+const Inicio = () => {
+  return (
+    <>
+        <div className='containerIncio'>
+            <h2 className='presentacion'>Inicio</h2>
+            <p className='texto'>Bienvenido a tu disquetera, pulse en cualquier parte para continuar.</p>
+        </div>
+    </>
+  )
+}
+
+export default Inicio
