45f4885a2ad69423c79482e5d860168d5a8b4c10
Roria <roriarumanosky@gmail.com>
2025-11-22 15:27:53 +0100

    Seguir haciendo 5.06

--- React/disquetera/src/Componentes/Formulario/Formulario.jsx
+++ React/disquetera/src/Componentes/Formulario/Formulario.jsx
@@ -1,38 +1,73 @@
 "use strict"
 
-import React from 'react'
+import React, { useState } from 'react'
 import "./Formulario.css"
 
 const Formulario = () => {
 
+    const [disco, setDisco] = useState({
+    nombreDisco : "",
+    caratulaDisco: "",
+    nombreGrupo : "",
+    yearPublication: "",
+    genero : "",
+    localizacionCodigo : "",
+    prestado :""
+    })
+
+    const [listadoDiscos, setListadoDiscos] = useState(
+        JSON.parse(localStorage.getItem("discos")) || []
+    )
+
+    const actualizarInputs = (e) => {
+        const {name, value} = e.target
+        setDisco(nprev => ({...nprev, [name]: value}))
+    }
+
+    const guardar = () => {
+        const nuevo = {...disco, id:crypto.randomUUID()}
+        const actualizar = [...listadoDiscos, nuevo]
+        setListadoDiscos(actualizar)
+        localStorage.setItem("discos",JSON.stringify(actualizar))
+        setDisco({
+            nombreDisco : "",
+            caratulaDisco: "",
+            nombreGrupo : "",
+            yearPublication: "",
+            genero : "",
+            localizacionCodigo : "",
+            prestado :"",
+        })
+    }
+    
   return (
-    <div>        
+    <div>
         <div id="mensajeError"></div>
-        <form id="formulario" name="formulario">
+        <form id="formulario">
             
             <div className="nombre">
                 <label htmlFor="nombreDisco"><h3>Nombre Disco</h3></label>
-                <input type="text" name="nombreDisco" id="nombreDisco" placeholder="Diggy Diggy Hole" required />
+                <input type="text" name="nombreDisco" id="nombreDisco" placeholder="Diggy Diggy Hole" required onChange={actualizarInputs} value={disco.nombreDisco}/>
             </div>
 
             <div className="imagenDisco">
                 <label htmlFor="caratulaDisco"><h3>Caratula del Disco</h3></label>
-                <input type="url" name="caratula" id="caratulaDisco" placeholder="Introduzca url de la caratula" />
+                <input type="url" id="caratulaDisco" name="caratulaDisco" placeholder="Introduzca url de la caratula" onChange={actualizarInputs} value={disco.caratulaDisco}/>
             </div>
 
             <div className="grupo">
                 <label htmlFor="nombreGrupo"><h3>Grupo de Música</h3></label>
-                <input type="text" name="nombreGrupo" id="nombreGrupo" placeholder="Wind Rose" />
+                <input type="text" name="nombreGrupo" id="nombreGrupo" placeholder="Wind Rose" onChange={actualizarInputs} value={disco.nombreGrupo}/>
             </div>
 
             <div className="yearPb">
                 <label htmlFor="yearPublication"><h3>Año de Publicación</h3></label>
-                <input type="number" name="yearPublication" id="yearPublication" placeholder="2019" />
+                <input type="number" name="yearPublication" id="yearPublication" placeholder="2019" onChange={actualizarInputs} value={disco.yearPublication}/>
             </div>
 
             <div className="generoOpcion">
                 <label htmlFor="genero"><h3>Género de Música</h3></label>
-                <select name="genero" id="genero">
+                <select name="genero" id="genero" onChange={actualizarInputs} value={disco.genero}>
                     <option value="">Selecciona una opción</option>
                     <option value="folk_metal">Folk Metal</option>
                     <option value="hardstyle">Hardstyle</option>
@@ -43,23 +78,22 @@ const Formulario = () => {
 
             <div className="localizacion">
                 <label htmlFor="localizacionCodigo"><h3>Localización</h3></label> 
-                <input type="text" name="localizacionCodigo" id="localizacionCodigo" />
+                <input type="text" name="localizacionCodigo" id="localizacionCodigo" onChange={actualizarInputs} value={disco.localizacionCodigo}/>
             </div>
             
             <div className="prestado">
                 <label><h3>Prestado</h3></label>
 
-                <input type="radio" name="prestado" id="prestado1" value="true" />
+                <input type="radio" name="prestado" id="prestado1" onChange={actualizarInputs} value="true" />
                 <label htmlFor="prestado1">Sí</label>
 
-                <input type="radio" name="prestado" id="prestado2" value="false" />
+                <input type="radio" name="prestado" id="prestado2" onChange={actualizarInputs} value="false" />
                 <label htmlFor="prestado2">No</label>
             </div>
 
             <div className="botones"><br />
-                <button id="guardar" type="button">Guardar</button>
+                <input type="button" id='guardar' value="Guardar" onClick={guardar}/>
             </div>
-
         </form>
     </div>
   )
--- React/disquetera/src/Componentes/Formulario/VerDiscos.jsx
+++ React/disquetera/src/Componentes/Formulario/VerDiscos.jsx
@@ -1,9 +1,46 @@
 "use strict";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./VerDiscos.css"
+
 
 const VerDiscos = () => {
+
+  const [listadoDiscos, setListadoDiscos] = useState([]);
+
+  useEffect(() => { 
+    const datos = JSON.parse(localStorage.getItem("discos")) || []
+    setListadoDiscos(datos)
+  }, [])
   return (
-    <div>VerDiscos</div>
+    <div className='contenedorDiscos'>
+      <h2>Listado Discos</h2>
+      {listadoDiscos.length === 0 && <p>No hay discos guardados</p>}
+    <table className='discoVer'>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Caratula</th>
+          <th>Grupo</th>
+          <th>Año</th>
+          <th>Código</th>
+          <th>Prestado</th>
+      </tr>
+      </thead>
+
+      <tbody>
+          {listadoDiscos.map((disco) => (
+            <tr key={disco.id}>
+                <td>{disco.nombreDisco}</td>
+                <td> <img src={disco.caratulaDisco} className='imagenCaratula'/> </td>
+                <td>{disco.nombreGrupo}</td>
+                <td>{disco.yearPublication}</td>
+                <td>{disco.localizacionCodigo}</td>
+                <td>{disco.prestado} </td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+    </div>
   )
 }
 
