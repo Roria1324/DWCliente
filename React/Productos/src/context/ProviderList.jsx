18d8a1e333662f898ece21d07d0c0f6c28a292a9
alex <ciclost@localhost>
2026-02-10 17:45:41 +0100

    Empezado 6.10

--- React/Productos/src/context/ProviderList.jsx
+++ React/Productos/src/context/ProviderList.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { createContext, useState } from "react";
+import { supabaseConnection } from "../supabase/supabase";
 
-const ProviderList = () => {
+const ListContext = createContext();
+
+const ProviderList = ({ children }) => {
+  const [lists, setLists] = useState([]);
+  const [error, setError] = useState(null);
+
+  const getList = async (userId) => {
+    setError(null);
+    try {
+      const { data, error } = await supabaseConnection
+        .from("lista_compra")
+        .select("*")
+        .eq("propietario_id", userId)
+        .order("created_at", { ascending: true });
+
+      if (!data) throw error;
+
+      setLists(data);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const createList = async (name, userId) => {
+    try {
+      const { data, error } = await supabaseConnection
+        .from("lista_compra")
+        .insert([{ nombre: name, propietario_id: userId }])
+        .select()
+        .single();
+
+      if (!data) throw error;
+
+      setLists((prev) => [data, ...prev]);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const deleteList = async (listId) => {
+    try {
+      const { data, error } = await supabaseConnection
+        .from("lista_compra")
+        .delete("id")
+        .eq("id", listId);
+      if (!data) throw error;
+      getList();
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const elements = {
+    getList,
+    createList,
+  };
   return (
-    <div>ProviderList</div>
-  )
-}
+    <ProviderList.Provider value={elements}>{children}</ProviderList.Provider>
+  );
+};
 
-export default ProviderList
+export default ProviderList;
+export { ListContext };
--- React/Productos/src/hooks/useList.js
+++ React/Productos/src/hooks/useList.js
@@ -1,13 +1,13 @@
-/*
-import { useContext } from "react";
-import {  } from "../context/ProviderList.jsx";
 
-const useProducts = () => {
-  const context = useContext();
+import { useContext } from "react";
+import { ListContext } from "../context/ProviderList.jsx";
+
+const useList = () => {
+  const context = useContext(ListContext);
 
   if (!context) {
     throw new Error("Error: Could not retrieve the context.");
   }
   return context;
 };
-export default ;*/
+export default useList;
